388427619d618f7606b86b9377dbd3abe5ccfcdb
ALeX <alexdeoliveira.poli@gmail.com>
2021-09-27 17:40:01 -0300

    Adding Codable conformance for @Published properties – Cupcake Corner SwiftUI Tutorial 1/8

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -2,10 +2,32 @@
 //  ContentView.swift
 //  CupcakeCorner
 //
-//  Created by Alex Oliveira on 27/09/21.
+//  Created by Alex Oliveira on 27/09/2021.
 //
 
 import SwiftUI
+
+class User: ObservableObject, Codable {
+	enum CodingKeys: CodingKey {
+		case name
+	}
+	
+	@Published var name = "Alex Oliveira"
+	// When wrapped with @Published, a String doesn't conform to Codable anymore. It's required to add:
+	// An enum conforming to CodingKey with a case for each wrapped property,
+	// A custom initializer (decoder)
+	// And a custom encoder function
+	
+	required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		name = try container.decode(String.self, forKey: .name)
+	}
+	
+	func encode(to encoder: Encoder) throws {
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		try container.encode(name, forKey: .name)
+	}
+}
 
 struct ContentView: View {
     var body: some View {
