0a4a7cd03b232bc4d973c7ccfc0c099dd03f941a
ALeX <alexdeoliveira.poli@gmail.com>
2021-09-27 20:42:21 -0300

    iTunes search for all songs of an artist given name

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,33 +7,73 @@
 
 import SwiftUI
 
+struct Response: Codable {
+	var results: [Result]
+}
+
+struct Result: Codable {
+	var trackId: Int
+	var trackName: String
+	var collectionName: String
+}
+
 struct ContentView: View {
-	@State var username = ""
-	@State var email = ""
-	
-	var disableForm: Bool {
-		username.count < 5 || email.count < 5
+	@State var results = [Result]()
+	@State var artistName = ""
+	var cleanedArtistName: String {
+		artistName.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: " ", with: "+")
 	}
 	
-    var body: some View {
-		Form {
-			Section {
-				TextField("Username", text: $username)
-				TextField("Email", text: $email)
+	var body: some View {
+		NavigationView {
+			VStack {
+				TextField("Artist Name", text: $artistName, onCommit: loadData)
+					.textFieldStyle(RoundedBorderTextFieldStyle())
+					.padding()
+					.onTapGesture { artistName = "" }
+				
+				List(results, id:\.trackId) { item in
+					VStack(alignment: .leading) {
+						Text(item.trackName)
+							.font(.headline)
+						
+						Text(item.collectionName)
+					}
+				}
+	//			.onAppear(perform: loadData)
 			}
-			
-			Section {
-				Button("Create account") {
-					print("Creating account...")
+			.navigationTitle(Text("All The Songs"))
+		}
+	}
+	
+	func loadData() {
+		guard let url = URL(string: "https://itunes.apple.com/search?term=\(cleanedArtistName)&entity=song") else {
+			print("Invalid URL")
+			artistName = "Invalid URL"
+			return
+		}
+		
+		let request = URLRequest(url: url)
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			if let data = data {
+				if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+					DispatchQueue.main.async {
+						self.results = decodedResponse.results
+					}
+					
+					return
 				}
 			}
-			.disabled(disableForm)
-		}
-    }
+			
+			print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+			artistName = "Fetch failed: \(error?.localizedDescription ?? "Unknown error")"
+		}.resume()
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
