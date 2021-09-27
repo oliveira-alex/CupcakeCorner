c87cddbdeacdf3966f574730197f5e04bfd6c325
ALeX <alexdeoliveira.poli@gmail.com>
2021-09-27 11:46:04 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alex Oliveira on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
