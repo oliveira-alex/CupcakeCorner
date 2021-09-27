849eb9ad13193bbac8d9b9387b2b7030c1e47f67
ALeX <alexdeoliveira.poli@gmail.com>
2021-09-27 19:27:38 -0300

    Validating and disabling forms – Cupcake Corner SwiftUI Tutorial 3/8

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,53 +7,29 @@
 
 import SwiftUI
 
-struct Response: Codable {
-	var results: [Result]
-}
-
-struct Result: Codable {
-	var trackId: Int
-	var trackName: String
-	var collectionName: String
-}
-
 struct ContentView: View {
-	@State var results = [Result]()
+	@State var username = ""
+	@State var email = ""
+	
+	var disableForm: Bool {
+		username.count < 5 || email.count < 5
+	}
 	
     var body: some View {
-		List(results, id:\.trackId) { item in
-			VStack(alignment: .leading) {
-				Text(item.trackName)
-					.font(.headline)
-				
-				Text(item.collectionName)
-			}
-		}
-		.onAppear(perform: loadData)
-    }
-	
-	func loadData() {
-		guard let url = URL(string: "https://itunes.apple.com/search?term=coldplay&entity=song") else {
-			print("Invalid URL")
-			return
-		}
-		
-		let request = URLRequest(url: url)
-		
-		URLSession.shared.dataTask(with: request) { data, response, error in
-			if let data = data {
-				if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-					DispatchQueue.main.async {
-						self.results = decodedResponse.results
-					}
-					
-					return
-				}
+		Form {
+			Section {
+				TextField("Username", text: $username)
+				TextField("Email", text: $email)
 			}
 			
-			print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-		}.resume()
-	}
+			Section {
+				Button("Create account") {
+					print("Creating account...")
+				}
+			}
+			.disabled(disableForm)
+		}
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
