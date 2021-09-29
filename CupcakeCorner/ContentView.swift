73f32a6d07057c97d69d1855a5da0bc9d60a18d0
ALeX <alexdeoliveira.poli@gmail.com>
2021-09-29 16:41:51 -0300

    Taking basic order details – Cupcake Corner SwiftUI Tutorial 4/8

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,68 +7,48 @@
 
 import SwiftUI
 
-struct Response: Codable {
-	var results: [Result]
-}
-
-struct Result: Codable {
-	var trackId: Int
-	var trackName: String
-	var collectionName: String
-}
-
 struct ContentView: View {
-	@State var results = [Result]()
-	@State var artistName = ""
-	var cleanedArtistName: String {
-		artistName.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: " ", with: "+")
-	}
+	@ObservedObject var order = Order()
 	
 	var body: some View {
 		NavigationView {
-			VStack {
-				TextField("Artist Name", text: $artistName, onCommit: loadData)
-					.textFieldStyle(RoundedBorderTextFieldStyle())
-					.padding()
-					.onTapGesture { artistName = "" }
-				
-				List(results, id:\.trackId) { item in
-					VStack(alignment: .leading) {
-						Text(item.trackName)
-							.font(.headline)
-						
-						Text(item.collectionName)
-					}
-				}
-	//			.onAppear(perform: loadData)
-			}
-			.navigationTitle(Text("All The Songs"))
-		}
-	}
-	
-	func loadData() {
-		guard let url = URL(string: "https://itunes.apple.com/search?term=\(cleanedArtistName)&entity=song") else {
-			print("Invalid URL")
-			artistName = "Invalid URL"
-			return
-		}
-		
-		let request = URLRequest(url: url)
-		
-		URLSession.shared.dataTask(with: request) { data, response, error in
-			if let data = data {
-				if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-					DispatchQueue.main.async {
-						self.results = decodedResponse.results
+			Form {
+				Section {
+					Picker("Select your cake type", selection: $order.type) {
+						ForEach(0..<Order.types.count) {
+							Text(Order.types[$0])
+						}
 					}
 					
-					return
+					Stepper(value: $order.quantity, in: 3...20) {
+						Text("number of cakes: \(order.quantity)")
+					}
+				}
+				
+				Section {
+					Toggle(isOn: $order.specialRequestEnabled.animation()) {
+						Text("Any special requests?")
+					}
+					
+					if order.specialRequestEnabled {
+						Toggle(isOn: $order.extraFrosting) {
+							Text("Add extra frosting")
+						}
+						
+						Toggle(isOn: $order.addSprinkles) {
+							Text("Add extra sprinkles")
+						}
+					}
+				}
+				
+				Section {
+					NavigationLink(destination: AddressView(order: order)) {
+						Text("Delivery details")
+					}
 				}
 			}
-			
-			print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-			artistName = "Fetch failed: \(error?.localizedDescription ?? "Unknown error")"
-		}.resume()
+			.navigationBarTitle("Cupcake Corner")
+		}
 	}
 }
 
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Alex Oliveira on 29/09/21.
+//
+
+import Foundation
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
